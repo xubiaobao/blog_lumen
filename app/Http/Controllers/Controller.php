3dab0c1b804cab8ce1ac4117487056836e257c27
641804809 <641804809@qq.com>
2023-05-10 11:31:28 +0800

    修改天气api

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -24,6 +24,11 @@ class Controller extends BaseController
         if ($locationInfo['status'] === 0) {
             $data['city'] = $locationInfo['result']['ad_info']['city'];
             $data['adcode'] = $locationInfo['result']['ad_info']['adcode'];
+            // 获取缓存
+            $cache = Redis::get('weather_' . $data['adcode']);
+            if (!is_null($cache)) {
+                return json_encode(['code' => 1, 'data' => json_decode($cache, true)]);
+            }
             // 获取城市编码
             $weatherKey = '06ea91a7abc14780b8b70fd701d1bf5d'; // 和风天气密钥
             $cirtUrl = 'https://geoapi.qweather.com/v2/city/lookup'; // 城市查找
@@ -34,11 +39,6 @@ class Controller extends BaseController
             }
             $cityId = $cityInfo['location'][0]['id'];
             $data['cityId'] = $cityId;
-            // 获取缓存
-            $cache = Redis::get('weather_'.$cityId);
-            if(!is_null($cache)){
-                return json_encode(['code' => 1, 'data' => json_decode($cache,true)]);
-            }
             // 获取天气
             $weatherUrl = 'https://devapi.qweather.com/v7/weather/now?location=' . $data['cityId'] . '&key=' . $weatherKey;
             $weatherInfo = file_get_contents($weatherUrl);
@@ -50,7 +50,7 @@ class Controller extends BaseController
             $data['text'] = $weatherInfo['now']['text'];
             $data['windDir'] = $weatherInfo['now']['windDir'];
             $data['windScale'] = $weatherInfo['now']['windScale'];
-            Redis::setex('weather_'.$cityId, 3600, json_encode($data));
+            Redis::setex('weather_' . $data['adcode'], 3600, json_encode($data));
             return json_encode(['code' => 1, 'data' => $data]);
         }
         return json_encode(['code' => 0, 'data' => []]);
